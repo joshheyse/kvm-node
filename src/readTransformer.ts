b53e43c1a34b27767e66950b25fa00c9a1462db3
Josh Heyse <josh@heyse.us>
2021-02-05 16:02:46 -0600

    update

--- src/readTransformer.ts
+++ src/readTransformer.ts
@@ -1,15 +1,12 @@
 import {Transform, TransformCallback} from 'stream';
 
 export class ReadTransform extends Transform {
-  private buffer = '';
+  private buf: Buffer = new Buffer(0);
 
   public _transform(chunk: Buffer, _encoding: BufferEncoding, callback: TransformCallback): void {
-    const str = chunk.toString('ascii');
-    this.buffer += str;
-    const crIndex = this.buffer.indexOf('\r');
-    if (crIndex) {
-      console.log('CR INDEX', crIndex);
-    }
+    this.buf = Buffer.concat([this.buf, chunk]);
+    console.log(this.buf.toString('ascii'));
+    console.log(this.buf.toString('hex'));
     callback();
   }
 }
