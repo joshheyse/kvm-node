f3b3f3bd8478cb2532dcfbde298c874454a6823b
Josh Heyse <josh@heyse.us>
2021-02-05 16:09:17 -0600

    added change port

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import SerialInterface from './serialInterface';
-import {ListInfoCommand} from './commands';
+import {ListInfoCommand, ChangePort} from './commands';
 
 async function main() {
   const kvmA = new SerialInterface('/dev/ttyUSB0');
   await kvmA.open();
   await kvmA.sendCommand(ListInfoCommand);
+  await kvmA.sendCommand(ChangePort(1));
 
   const kvmB = new SerialInterface('/dev/ttyUSB1');
   await kvmB.open();
