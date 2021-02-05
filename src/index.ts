6d74e56a83164c9edebb0c4b99b6d17be92b1223
Josh Heyse <josh@heyse.us>
2021-02-05 15:30:54 -0600

    update

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
 import SerialInterface from './serialInterface';
 import {ListInfoCommand} from './commands';
 
-async function sleep(ms: number): Promise<void> {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
-
 async function main() {
   const kvmA = new SerialInterface('/dev/ttyUSB0');
   await kvmA.open();
